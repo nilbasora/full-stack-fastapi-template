72360536695006578dd888c1f1456059475c0a7b
Nil Basora <32940624+nilbasora@users.noreply.github.com>
2025-11-06 19:04:21 +0100

    Add test_develop endpoint for health check

--- backend/app/api/routes/utils.py
+++ backend/app/api/routes/utils.py
@@ -29,3 +29,7 @@ def test_email(email_to: EmailStr) -> Message:
 @router.get("/health-check/")
 async def health_check() -> bool:
     return True
+
+@router.get("/health-check/")
+async def test_develop() -> bool:
+    return True
